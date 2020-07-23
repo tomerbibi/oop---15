0ff499c56fa4a1065a915c9b8f6456fe05a682de
tomerbibi <66681821+tomerbibi@users.noreply.github.com>
2020-07-23 16:45:00 +0300

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_15
+{
+    class Game
+    {
+        public string _name;
+        public int _numberOfPlayers;
+        public float _rating;
+        public string _originCountry;
+
+        public Game()
+        {
+        }
+        public Game(string name, float rating = 5)
+        {
+            _name = name;
+            _rating = rating;
+        }
+        public Game (string name, int numberOfPlayers, float rating, string originCountry)
+        {
+            _name = name;
+            _numberOfPlayers = numberOfPlayers;
+            _rating = rating;
+            _originCountry = originCountry;
+        }
+        public Game (string name, string originCountry)
+        {
+            _name = name;
+            _originCountry = originCountry;
+        }
+        public int TellMeHowManyPlayers()
+        {
+            return _numberOfPlayers;
+        }
+
+        public override string ToString()
+        {
+            return $"name: {_name}, number of players: {_numberOfPlayers}, rating {_rating}, original country: {_originCountry}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_15
+{
+    class Program
+    {
+        static void PrintGame(Game g)
+        {
+            Console.WriteLine(g.ToString());
+            int number = g.TellMeHowManyPlayers();
+        }
+        static void Main(string[] args)
+        {
+            Game g = new Game();
+            Game socker = new Game("socker", 8f);
+            Game snooker = new Game("snooker", 2, 5, "england");
+            Game tennis = new Game("tennis", "france");
+
+        }
+    }
+}
